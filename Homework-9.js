e100912c04f214388a6c91fd7fa0d923b8948672
alamg <aali0051@student.monash.edu>
2025-10-12 23:01:08 +1100

    Homework 9 update
    Testing if that still works

--- Homework-9.js
+++ Homework-9.js
@@ -1,5 +1,5 @@
 var vg_1 = "week_9_HW.vl.json";
 
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#vis1", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
     }).catch(console.error);
