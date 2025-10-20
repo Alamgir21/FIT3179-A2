b0ff08bc585fb542a53519e71be4fed067f8b31d
alamg <aali0051@student.monash.edu>
2025-10-20 21:00:05 +1100

    All idioms are now on the html file.
    Now i need to edit the formatting and then tommorow makes changes to the graphs

---
+++ Idiom-Loader.js
@@ -0,0 +1,11 @@
+var vg_1 = "World map and total tourist bar graph.vega";
+var vg_2 = "Regional idioms.vega";
+var vg_3 = "Pie chart of GDP.vega";
+var vg_4 = "Jobs line chart.vega";
+var vg_5 = "Jobs by industry bar chart.vega";
+
+vegaEmbed("#vis1",vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis2",vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis3",vg_3).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis4",vg_4).then(function(result) {}).catch(console.error);
+vegaEmbed("#vis5",vg_5).then(function(result) {}).catch(console.error);
