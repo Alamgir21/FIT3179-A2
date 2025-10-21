9faedb74864df4f2cdaf9fe7bff869234c3f071f
alamg <aali0051@student.monash.edu>
2025-10-21 18:36:22 +1100

    Styling has been updated to centre all elements and give it a better look. Map chart is also fixed with its annotation.
    Now going to add annotations on the bar chart and Australian map

--- Idiom-Loader.js
+++ Idiom-Loader.js
@@ -1,11 +1,13 @@
-var vg_1 = "World map and total tourist bar graph.vega";
-var vg_2 = "Regional idioms.vega";
-var vg_3 = "Pie chart of GDP.vega";
-var vg_4 = "Jobs line chart.vega";
-var vg_5 = "Jobs by industry bar chart.vega";
+var WorldMap = "World map.vega";
+var TouristsTotal = "Tourists total.vega";
+var RegionalIdioms = "Regional idioms.vega";
+var GDPPie = "Pie chart of GDP.vega";
+var JobsLineChart = "Jobs line chart.vega";
+var JobsBarChart = "Jobs by industry bar chart.vega";
 
-vegaEmbed("#vis1",vg_1).then(function(result) {}).catch(console.error);
-vegaEmbed("#vis2",vg_2).then(function(result) {}).catch(console.error);
-vegaEmbed("#vis3",vg_3).then(function(result) {}).catch(console.error);
-vegaEmbed("#vis4",vg_4).then(function(result) {}).catch(console.error);
-vegaEmbed("#vis5",vg_5).then(function(result) {}).catch(console.error);
+vegaEmbed("#WorldMap",WorldMap).then(function(result) {}).catch(console.error);
+vegaEmbed("#TouristsTotal",TouristsTotal).then(function(result) {}).catch(console.error);
+vegaEmbed("#RegionalIdioms",RegionalIdioms).then(function(result) {}).catch(console.error);
+vegaEmbed("#GDPPie",GDPPie).then(function(result) {}).catch(console.error);
+vegaEmbed("#JobsLineChart",JobsLineChart).then(function(result) {}).catch(console.error);
+vegaEmbed("#JobsBarChart",JobsBarChart).then(function(result) {}).catch(console.error);
